e83d8452bfc8a7c0be0e1d7f8ce1bfff1efc4acf
igoldsteinh <61714888+igoldsteinh@users.noreply.github.com>
2023-11-17 14:39:42 -0800

    update initial conditions

--- scripts/pull_ww_data.R
+++ scripts/pull_ww_data.R
@@ -166,6 +166,7 @@ cases <-
   arrange(date, county)
 
 ca_cases <- cases %>% 
+            filter(county != "Out of state") %>%
             group_by(date) %>% 
             summarise(
               cases = sum(cases),
@@ -176,7 +177,7 @@ ca_cases <- cases %>%
 
 full_cases <- bind_rows(cases, ca_cases)
 
-start_date <- fitting_dat %>% 
+start_date <- full_fitting_dat %>% 
               group_by(county) %>% 
               filter(date == min(date)) %>% 
               dplyr::select(county, date) %>% 
